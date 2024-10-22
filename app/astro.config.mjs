19b90700fa2414531c2409e30676870ebaf77db7
Yuta <yutalinux@users.noreply.github.com>
2024-10-22 16:09:36 +0900

    update

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -4,5 +4,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://yutalinux.github.io",
+  base: "/tech-blog",
   integrations: [tailwind()],
 });
---
+++ contents/src/index.ts
@@ -0,0 +1,69 @@
+import { readdirSync, readFileSync, statSync } from "fs";
+import matter from "gray-matter";
+import * as path from "path";
+
+// ブログの記事データを入れるディレクトリ
+export const POST_DIR = "posts";
+
+/**
+ * 記事データの型
+ */
+export interface Post {
+  /**
+   * 記事のID
+   */
+  slug: string;
+
+  /**
+   * 記事のタイトル
+   */
+  title: string;
+
+  /**
+   * 説明
+   */
+  description: string;
+
+  /**
+   * 公開日
+   */
+  pubDate: string;
+
+  /**
+   * 更新日
+   */
+  updatedDate: string;
+
+  /**
+   * タグ
+   */
+  tags: string[];
+
+  /**
+   * マークダウンの本文
+   */
+  body: string;
+}
+
+/**
+ * ブログの記事一覧を取得する
+ * @returns 記事の一覧データ
+ */
+export function getPosts(): Post[] {
+  // @ts-ignore
+  const dirname = import.meta.dirname;
+  const packageDir = path.resolve(dirname, "..");
+
+  const posts = readdirSync(path.resolve(packageDir, POST_DIR));
+  return posts.map((post) => {
+    const fp = path.resolve(packageDir, POST_DIR, post);
+    const parsePath = path.parse(fp);
+    const stat = statSync(fp);
+    // if (parsePath.ext !== "md" || !stat.isFile()) return;
+    const { data, content } = matter({ content: readFileSync(fp, "utf-8") });
+    let result = data as Post;
+    result.slug = parsePath.name;
+    result.body = content;
+    return result;
+  });
+}
