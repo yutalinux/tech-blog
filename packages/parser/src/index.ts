82e10db192a4b73db135c43089af3c6da9b1a2b3
Yuta <yutalinux@users.noreply.github.com>
2024-10-23 09:23:36 +0900

    update

--- packages/parser/src/index.ts
+++ packages/parser/src/index.ts
@@ -1,13 +1,13 @@
 import { remark } from "remark";
 import remarkGfm from "remark-gfm";
 import remarkDirective from "remark-directive";
-import remarkMessagePlugin from "@repo/remark-message-plugin";
+import remarkMessagePlugin from "../../remark-note-plugin/src";
 import remark2rehype from "remark-rehype";
 import html from "rehype-stringify";
 import rehypeHighlight from "rehype-highlight";
 import rehypeHighlightCodeLines from "rehype-highlight-code-lines";
 
-export async function markdownToHtml(markdown: string): Promise<string> {
+export default async function markdown2html(markdown: string): Promise<string> {
   const result = await remark()
     .use(remarkDirective)
     .use(remarkMessagePlugin)
