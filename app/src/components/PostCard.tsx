8d13bc76f37728ec3307d52e69b32af79ce7076b
yuta <jp.yuta0131@gmail.com>
2024-10-24 10:20:33 +0900

    update

--- app/src/components/PostCard.tsx
+++ app/src/components/PostCard.tsx
@@ -8,7 +8,7 @@ export default function PostCard({ slug, title, description }: Props) {
   return (
     <a
       href={`/tech-blog/${slug}`}
-      className="flex flex-col w-64 h-48 border p-2 rounded-md"
+      className="flex flex-col w-full h-32 border p-2 rounded-md"
     >
       <h2 className="text-2xl">{title}</h2>
       <p>{description}</p>
